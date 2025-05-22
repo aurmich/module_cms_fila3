3cc1ff19aa00248aac609fd8980b38fd813bfd10
marco76tv <marco.sottana@gmail.com>
2025-05-22 10:22:06 +0200

    refactor(NetfunSmsMessage.php): rename class to NetfunSMSMessage and update properties for clarity and consistency feat(NetfunSmsMessage.php): add fromArray method to facilitate object creation from an array

--- laravel/Modules/Notify/app/Datas/NetfunSmsMessage.php
+++ laravel/Modules/Notify/app/Datas/NetfunSmsMessage.php
@@ -1,16 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Notify\Datas;
 
 use Spatie\LaravelData\Data;
 
-class NetfunSmsMessage extends Data
+class NetfunSMSMessage extends Data
 {
     public function __construct(
-        public string $recipient,
+        public string $to,
         public string $text,
-        public string $sender,
+        public string $from,
         public ?string $reference = null,
-        public ?string $scheduledDate = null,
-    ) {}
+        public ?string $scheduled_date = null
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            to: $data['to'],
+            text: $data['text'],
+            from: $data['from'],
+            reference: $data['reference'] ?? null,
+            scheduled_date: $data['scheduled_date'] ?? null
+        );
+    }
 }
