53ac6100b05ec1d8a55c6dae0e937c129ac7515b
c.rubiero <c.rubiero@exabytesrl.it>
2025-06-05 10:16:54 +0200

    - Rename file: header, footer, home; - Aggiunto stile alla pagina del dettaglio paziente;

--- laravel/Themes/One/resources/views/pages/classi-css.blade.php
+++ laravel/Themes/One/resources/views/pages/classi-css.blade.php
@@ -43,4 +43,10 @@
 <div class="!border-black">BorderBlack</div>
 <div class="mb-12">Margin bottom</div>
 <div class="m-6">Margin</div>
+<div class="w-96">Width</div>
+<div class="bg-[#B9C3D1]"></div>
+<div class="p-8">Padding</div>
+<div class="mb-8">Marginnn</div>
+<div class="!py-8">Padding zero</div>
+<div class="bg-[#DBE3EE]">Background button azioni</div>
 </x-layouts.app>
