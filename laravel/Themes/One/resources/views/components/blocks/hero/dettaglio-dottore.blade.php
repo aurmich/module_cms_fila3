4647762139c6e3c99262585e49e0f2ca03188722
c.rubiero <c.rubiero@exabytesrl.it>
2025-06-13 17:44:43 +0200

    - update contenuto pagine appuntamento - redirect dalla pagina di appuntamento al dettaglio del dottore

--- laravel/Themes/One/resources/views/components/blocks/hero/dettaglio-dottore.blade.php
+++ laravel/Themes/One/resources/views/components/blocks/hero/dettaglio-dottore.blade.php
@@ -457,12 +457,14 @@
           </a>
           </div>
           <div class="overflow-hidden rounded-lg bg-white shadow m-5">
-             <div class="bg-[#F38B8B] px-4 py-5 sm:p-6 flex flex-row">
+            <a href="/it/pages/appuntamenti-rifiutati">
+            <div class="bg-[#F38B8B] px-4 py-5 sm:p-6 flex flex-row">
              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#BF0303" class="size-6">
                <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
               </svg>
               <span class="ml-2 text-[#BF0303]">Appuntamenti rifiutati</span>
              </div>
+            </a>
           </div>
       </div>
      </div>
