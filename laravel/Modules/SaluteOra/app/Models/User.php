2f169a46566dd11aa77b62c575a3b42340221c61
c.rubiero <c.rubiero@exabytesrl.it>
2025-06-25 17:45:35 +0200

    - create progetto page

--- laravel/Modules/SaluteOra/app/Models/User.php
+++ laravel/Modules/SaluteOra/app/Models/User.php
@@ -211,7 +211,7 @@ class User extends BaseUser implements HasMedia
         'password_expires_at',
         //'studio_id',
         //'continuation_token',
-        'certificates'
+        'certifications'
     ];
 
 
