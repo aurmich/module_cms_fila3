8d44f10deacdca8fe3c5647c73d5d07337b33090
marco76tv <marco.sottana@gmail.com>
2025-06-27 10:40:11 +0200

    feat: studio trans , radio collection

--- laravel/Modules/SaluteMo/lang/it/studio.php
+++ laravel/Modules/SaluteMo/lang/it/studio.php
@@ -169,6 +169,10 @@ return array (
     array (
       'label' => 'reorderRecords',
     ),
+    'resetFilters' => 
+    array (
+      'label' => 'resetFilters',
+    ),
   ),
   'actions' => 
   array (
