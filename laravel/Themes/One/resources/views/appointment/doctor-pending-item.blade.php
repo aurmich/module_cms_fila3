fe3a45b024f3b1ea5cb78a2cc7e25fca407dec73
c.rubiero <c.rubiero@exabytesrl.it>
2025-07-04 12:13:30 +0200

    - variazioni

--- laravel/Themes/One/resources/views/appointment/doctor-pending-item.blade.php
+++ laravel/Themes/One/resources/views/appointment/doctor-pending-item.blade.php
@@ -5,12 +5,10 @@
             <!-- Info -->
             <div class="flex flex-row items-center">
                 <div>
-                    <span class="text-lg">{{ $appointment->patient->full_name }}</span>
+                    <span class="text-lg">{{ $appointment->patient?->full_name }}</span>
                     <div>
                         <p class="text-xs">{{ $appointment->starts_at?->format('d/m/Y') }}</p>
                         <p class="text-xs">{{ $appointment->time_range }}</p>
-                        
-                       
                     </div>
                 </div>
             </div>
@@ -41,14 +39,14 @@
                         <div class="bg-white p-6 rounded-xl max-w-md w-3/4 lg:w-full">
                             <h2 class="text-lg font-semibold text-gray-800 mb-4">Dettagli Appuntamento</h2>
                             <div class="text-sm text-gray-700 space-y-2">
-                                <p><strong>Nome:</strong> {{ $appointment->patient->full_name }}</p>
+                                <p><strong>Nome:</strong> {{ $appointment->patient?->full_name }}</p>
                                 <p><strong>Data:</strong> {{ $appointment->starts_at?->format('d F Y') }}</p>
                                 <p><strong>Orario:</strong> {{ $appointment->time_range }}</p>
-                                @if($appointment->patient->phone)
-                                    <p><strong>Cellulare:</strong> {{ $appointment->patient->phone }}</p>
+                                @if($appointment->patient?->phone)
+                                    <p><strong>Cellulare:</strong> {{ $appointment->patient?->phone }}</p>
                                 @endif
-                                @if($appointment->patient->email)
-                                    <p><strong>Email:</strong> {{ $appointment->patient->email }}</p>
+                                @if($appointment->patient?->email)
+                                    <p><strong>Email:</strong> {{ $appointment->patient?->email }}</p>
                                 @endif
                                 @if($appointment->notes)
                                     <p><strong>Note:</strong> {{ $appointment->notes }}</p>
@@ -76,7 +74,7 @@
                       <div x-show="showInfo" x-cloak class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
                         <div class="bg-white p-6 rounded-xl max-w-md w-3/4 lg:w-full">
                             <h2 class="text-[#272C4D] text-lg font-semibold mb-4">Accetta Appuntamento</h2>
-                            <p class="text-sm text-gray-600">Sei sicuro di voler accettare l'appuntamento con {{ $appointment->patient->full_name }}?</p>
+                            <p class="text-sm text-gray-600">Sei sicuro di voler accettare l'appuntamento con {{ $appointment->patient?->full_name }}?</p>
                             <div class="mt-6 flex justify-end gap-2">
                                 <button @click="showInfo = false" class="px-4 py-2 bg-gray-200 rounded-md">
                                     Annulla
@@ -109,7 +107,7 @@
                     <div x-show="open" x-cloak class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
                         <div class="bg-white p-6 rounded-xl max-w-md w-3/4 lg:w-full">
                             <h2 class="text-[#272C4D] text-lg font-semibold mb-4">Rifiuta Appuntamento</h2>
-                            <p class="text-sm text-gray-600">Sei sicuro di voler rifiutare l'appuntamento con {{ $appointment->patient->full_name }}?</p>
+                            <p class="text-sm text-gray-600">Sei sicuro di voler rifiutare l'appuntamento con {{ $appointment->patient?->full_name }}?</p>
                             <div class="mt-6 flex justify-end gap-2">
                                 <button @click="open = false" class="px-4 py-2 bg-gray-200 rounded-md">
                                     Annulla
