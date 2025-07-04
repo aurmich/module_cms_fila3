4c10a60069926fc461630e9384e2f2784021be90
marco76tv <marco.sottana@gmail.com>
2025-07-04 13:11:21 +0000

    trans

---
+++ laravel/Modules/SaluteOra/lang/en/doctor_appointments.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+  'actions' => 
+  array (
+    'info' => 
+    array (
+      'label' => 'info',
+    ),
+    'confirmAction' => 
+    array (
+      'label' => 'confirmAction',
+    ),
+    'rejectAction' => 
+    array (
+      'label' => 'rejectAction',
+    ),
+  ),
+);
