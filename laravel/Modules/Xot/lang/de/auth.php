69bc9fb10c18ec16e50546ab04de6a14e51b70dd
marco76tv <marco.sottana@gmail.com>
2025-07-11 18:10:12 +0200

    translations

--- laravel/Modules/Xot/lang/de/auth.php
+++ laravel/Modules/Xot/lang/de/auth.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    'failed' => 'Zugangsdaten nicht gefunden.',
-    'general_error' => 'Du hast keine Berechtigung um dies zu machen.',
+    'failed' => 'Diese Anmeldedaten stimmen nicht mit unseren Aufzeichnungen überein!',
+    'general_error' => 'Sie haben nicht genügend Rechte für diese Operation.',
     'socialite' => [
-        'unacceptable' => ':provider ist kein akzeptierter Logintyp.',
+        'unacceptable' => ':provider wird nicht unterstützt.',
     ],
-    'throttle' => 'Zuviele Login versuche. Bitte warte :seconds Sekunden.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es in :seconds Sekunden erneut.',
     'unknown' => 'Ein unbekannter Fehler ist aufgetreten',
 ];
