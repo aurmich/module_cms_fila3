419b251f24992bf95e3c7632a8f919f7dd3967d3
marco76tv <marco.sottana@gmail.com>
2025-07-21 11:35:53 +0200

    ✨ (appuntamenti-conclusi.blade.php): add additional states to DoctorAppointmentsWidget to enhance appointment tracking functionality

--- laravel/Themes/One/resources/views/components/blocks/hero/appuntamenti-conclusi.blade.php
+++ laravel/Themes/One/resources/views/components/blocks/hero/appuntamenti-conclusi.blade.php
@@ -28,7 +28,10 @@ $user=auth()->user();
     </div>
     
       <div>
-    @livewire(\Modules\SaluteOra\Filament\Widgets\DoctorAppointmentsWidget::class, ['doctor_id' => $user->id,'states' => ['completed','report_completed']])
+    @livewire(\Modules\SaluteOra\Filament\Widgets\DoctorAppointmentsWidget::class, ['doctor_id' => $user->id,
+    'states' => ['completed','report_completed',
+    'refund_pending','refund_accepted','refund_to_integrate','refund_completed',
+    'pro_bono',]])
     </div>
     
 </div>
