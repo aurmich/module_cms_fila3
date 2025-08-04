729b503bac953410a97f76957a2c10c6f6d17409
marco76tv <marco.sottana@gmail.com>
2025-08-04 17:47:56 +0200

    rebase 300

--- laravel/test_calendar_methods.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Test file to check available methods in FullCalendarWidget
-require_once __DIR__ . '/vendor/autoload.php';
-
-$methods = get_class_methods('\Saade\FilamentFullCalendar\Widgets\FullCalendarWidget');
-var_dump($methods);
