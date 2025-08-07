bf621abe2c2087bd93d3533a1d6eccbf30fde7e5
marco76tv <marco.sottana@gmail.com>
2025-08-07 15:13:36 +0000

    feat: HTTP_X_FORWARDED_PROTO on xotdata

--- laravel/Modules/Xot/app/Datas/XotData.php
+++ laravel/Modules/Xot/app/Datas/XotData.php
@@ -363,6 +363,11 @@ class XotData extends Data implements Wireable
         if(isset($_SERVER['SERVER_NAME']) && '127.0.0.1' == $_SERVER['SERVER_NAME']){
             return false;
         }
+        //AWS ELB 
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            return true;
+        }
+        
         //if(isset($_SERVER['SERVER_NAME']) && Str::endsWith($_SERVER['SERVER_NAME'],'.local')){
         //    return false;
         //}
